e9ab6880984249f30e05513cedcfed676a417557
Jen <30688276+helplah@users.noreply.github.com>
2018-12-28 09:41:54 +0800

    add binaryGap.js and initialise repo

---
+++ Lesson 01 - Iterations/binaryGap.js
@@ -0,0 +1,23 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(N) {
+  let increment = 0;
+  let binaryGap = 0;
+  const binary = N.toString(2);
+  
+  for (let x = 0; x < binary.length; x++) {
+      if (binary[x] === "0") {
+          // increment = 3
+          increment++;
+      } else if (increment > binaryGap) {
+          // binaryGap = 5
+          binaryGap = increment;
+          increment = 0;
+      } else {
+          increment = 0;    
+      }
+  }
+
+  return binaryGap;
+}
